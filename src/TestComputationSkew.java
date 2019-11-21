5fbb22475ffc773810d7db31429d07892a646239
Jiyuan Wang <871973298@qq.com>
2019-11-20 17:13:07 -0800

    change read file

--- src/TestComputationSkew.java
+++ src/TestComputationSkew.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class TestComputationSkew {
 
     public static void main(String[] args) throws IOException {
-        String b = "chapter5/";
+        String b = "result-for-computation"; //change to your spark log file
         ArrayList<ComputationSkewFeedback> array= new ComputationSkewDetector(4,b,b,0.9).getEntropyArray();
 
     }
